03fb0fed288455a5fc42f9392bf9e8f6bab50287
JOSE GABRIEL <>
2023-03-09 21:48:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+    System.out.println("Digite um nome");
+
+    Scanner sc1 = new Scanner(System.in);
+
+    String nome = sc1.nextLine();
+
+    System.out.println("seu nome é: " + nome + "?");
+    
+    
+    
+  }
+}
